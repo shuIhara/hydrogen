979260a021427344b0dc14be9021e703a00255e9
ihara <ihara@metaphase.co.jp>
2023-09-03 21:20:57 +0900

    second

---
+++ app/routes/_index.jsx
@@ -0,0 +1,44 @@
+import {useLoaderData, Link} from '@remix-run/react';
+
+export function meta() {
+  return [
+    {title: 'Hydrogen'},
+    {description: 'A custom storefront powered by Hydrogen'},
+  ];
+}
+
+export async function loader({context}) {
+  return await context.storefront.query(COLLECTIONS_QUERY);
+}
+
+export default function Index() {
+  const {collections} = useLoaderData();
+  return (
+    <section className="w-full gap-4">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+        Collections
+      </h2>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+        {collections.nodes.map((collection) => {
+          return (
+            <Link to={`/collections/${collection.handle}`} key={collection.id}>
+              {collection.title}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+const COLLECTIONS_QUERY = `#graphql
+  query FeaturedCollections {
+    collections(first: 3, query: "collection_type:smart") {
+      nodes {
+        id
+        title
+        handle
+      }
+    }
+  }
+`;
